9e32049a7e754f868e1e59cccf5db8f4932aaa87
KaiserKatze <donizyo@gmail.com>
2018-09-24 23:09:38 +0800

    Upate core

--- app.py
+++ app.py
@@ -1,17 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+import signal
+
 from flask import Flask, render_template, url_for
 
-from . import futures
+from futures import SHFE
+
+###########
+
+shfe = None
+
+def handler(signal, frame):
+
+    shfe.saveTable()
+
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, handler)
+
+###########
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.temp.html', pathAppJs=url_for('static', filename='app.js'))
+    return render_template('index.temp.html',
+            pathAppJs=url_for('static', filename='app.js'),
+            pathAppCss=url_for('static', filename='app.css'),
+        )
 
 @app.route('/ajax/spider/start/', methods=['POST'])
 def ajax():
-    SHFE()
+    global shfe
+
+    shfe = SHFE()
+
+    try:
+        shfe.startSpider()
+    finally:
+        shfe.saveTable()
+
     return 'hello'
+
--- futures.py
+++ futures.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# `pip install requests`
-
-from datetime import datetime, date
-from dateutil.rrule import rrule, DAILY
+import os.path
+import datetime
+import dateutil.rrule as rrule
 import enum
 import json
+import sqlite3
+import collections
+import logging
+import typing
 
 import requests
 
@@ -14,6 +17,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+LOGGING_FILE = 'futures.log'
+logging.basicConfig(\
+        filename=LOGGING_FILE,
+        filemode='w',
+        level=logging.INFO,
+        format='[%(asctime)s] %(levelname)s %(message)s',
+        datefmt='%m/%d/%Y %I:%M:%S %p',
+    )
+logger = logging.getLogger(__name__)
 
 def Session(host, referer):
     headers = {
@@ -29,112 +41,204 @@ def Session(host, referer):
     }
     session = requests.Session()
     session.headers.update(headers)
+
     return session
 
 def plot(dataframe, instrumentId):
     df[df['instrumentId'] == instrumentId].plot(\
-        x='reportDate',
-        y='refSettlementPrice',
-        kind='line',
-        title=instrumentId,)
+            x='reportDate',
+            y='refSettlementPrice',
+            kind='line',
+            title=instrumentId,
+        )
     plt.show()
 
-class SHFE:
-        
-    class Suffix(enum.Enum):
-        sfe_default = 'defaultTimePrice.dat'
-        sfe_main    = 'mainTimePrice.dat'
-        sfe_daily   = 'dailyTimePrice.dat'
+class SpiderException(Exception):
+    pass
 
-    LOGGING_FILE = 'futures.log'
-    FUTURES_CSV = 'futures.csv'
-    INSTRUMENTIDS = ( 'CU', 'AL', 'ZN', 'PB', 'NI', 'SN', 'AU', 'AG', 'RB', 'WR', 'HC', 'FU', 'BU', 'RU', )
+class SHFE:
+
+    class Suffix(enum.Enum):
+        default = 'defaultTimePrice.dat'
+        main    = 'mainTimePrice.dat'
+        daily   = 'dailyTimePrice.dat'
+
+    PRODUCTIDS = ( 'CU', 'AL', 'ZN', 'PB', 'NI', 'SN', 'AU', 'AG', 'RB', 'WR', 'HC', 'FU', 'BU', 'RU', )
     HOSTNAME = 'www.shfe.com.cn'
     URL_REFERER = 'http://www.shfe.com.cn/statements/dataview.html?paramid=delaymarket_cu'
+    DEFAULT_INDEXES = ( 'instrumentId', 'refSettlementPrice', 'updown', )
+    DEFAULT_SQL_PATH = 'shfe.sqlite3'
+    DEFAULT_SQL_TABLE_NAME = 'shfe'
 
     def __init__(self):
+        pass
+
+    def startSpider(self):
         with Session(host=SHFE.HOSTNAME, referer=SHFE.URL_REFERER) as session:
-            df = self.traverseDate(lambda dt: self.fetchData(session, dt, Suffix.sfe_main))
+            try:
+                self.loadTable()
+                self.traverseDate(\
+                        callback=lambda dt: self.fetchData(session, dt, SHFE.Suffix.daily),
+                    )
+            finally:
+                self.saveTable()
 
-            date = datetime.strptime('2018-7-2', '%Y-%m-%d')
-            print('Date:', date.strftime('%Y-%m-%d'))
-            #self.fetchData(session, date, Suffix.sfe_default)
-            #self.fetchData(session, date, Suffix.sfe_daily)
-            self.fetchData(session, date, Suffix.sfe_main)
+    def loadTable(self):
+        self.table = None
 
-    def parseData(self, date, response):
-        result = json.loads(response.text)
-        indexes = ('reportDate', 'instrumentId', 'refSettlementPrice', 'updown')
+        if os.path.isfile(SHFE.DEFAULT_SQL_PATH):
+            self.table = self.loadTableFromSqlite()
 
-        with open(LOGGING_FILE, 'a') as log:
-            # 日期
-            reportDate = date or pd.to_datetime(result['report_date'], format='%Y%m%d', errors='ignore')
+        if self.table is None:
+            self.table = self.createNewTable()
 
-            table = pd.DataFrame(index=indexes)
-            for chunk in result['o_currefprice']:
+        assert self.table is not None, 'Fail to load table!'
 
-                if chunk.get('TIME') == '9:00-15:00':
+    def createNewTable(self):
+        return pd.DataFrame(columns=self.DEFAULT_INDEXES)
 
-                    # 品种
-                    instrumentId = chunk['INSTRUMENTID'][:2]
-                    assert instrumentId in INSTRUMENTIDS, 'Invalid `instrumentId`!'
+    def loadTableFromSqlite(self):
+        try:
+            with sqlite3.connect(SHFE.DEFAULT_SQL_PATH) as conn:
+                return pd.read_sql_query(\
+                        f'''SELECT *
+                        FROM {SHFE.DEFAULT_SQL_TABLE_NAME}''',
+                        conn,
+                    )
+            # TODO validate data integrity
+        except Exception as e:
+            logger.exception(e)
+            return None
 
-                    # 加权平均价
-                    refSettlementPrice = int(chunk['REFSETTLEMENTPRICE'])
+    def saveTable(self):
+        if self.table is None:
+            raise TypeError('Field `table` is None!')
 
-                    # 涨跌
-                    updown = float(chunk['UPDOWN'])
+        with sqlite3.connect(SHFE.DEFAULT_SQL_PATH) as conn:
+            self.table.to_sql(\
+                    SHFE.DEFAULT_SQL_TABLE_NAME,
+                    conn,
+                    if_exists='replace',
+                )
 
-                    #log.write('{}\t{}\t{}\t{}\n'.format(reportDate, instrumentId, refSettlementPrice, updown))
-                    row = pd.Series([reportDate, instrumentId, refSettlementPrice, updown,], index=indexes)
+    def parseChunk(self,
+                reportDate: datetime.date,
+                chunk,
+            ):
+        if not reportDate:
+            raise TypeError('Argument `reportDate` is not specified!')
+        if not isinstance(reportDate, datetime.date):
+            raise TypeError(f'Argument `reportDate` has invalid type: `{type(reportDate)}`!')
+        if not chunk:
+            raise TypeError('Argument `chunk` is not specified!')
 
-                    table.append(row)
+        u'''合约名称'''
+        instrumentId = chunk['INSTRUMENTID'].strip().upper()
+        if not instrumentId[:2] in SHFE.PRODUCTIDS:
+            raise SpiderException(f'Invalid `instrumentId`: {instrumentId}!')
 
-            #print(table)
-            #df = pd.DataFrame(table,
-            #    columns=['reportDate', 'instrumentId', 'refSettlementPrice', 'updown',],)
-            #print(df)
+        u'''加权平均价'''
+        refSettlementPrice = int(chunk['REFSETTLEMENTPRICE'])
 
-            #log.write('\n')
-            #log.write(json.dumps(result, sort_keys=False, indent=2, ensure_ascii=False))
+        u'''涨跌'''
+        updown = int(round(float(chunk['UPDOWN'])))
 
-            return table
+        u'''创建新记录（行）'''
+        row = pd.Series([
+                    instrumentId,
+                    refSettlementPrice,
+                    updown,
+                ],
+                index=self.DEFAULT_INDEXES,
+                name=pd.to_datetime(reportDate),
+            )
 
-    def fetchData(self, session, date, suffix):
-        url = self.generateUrl(date, suffix)
-        response = session.get(url)
-        if response.status_code == 200:
-            return self.parseData(date, response)
-        else:
-            return []
+        #logger.info(row)
 
-    def traverseDate(callback):
-        # 最早的记录，从2002年1月1日开始
-        dsrc = date(2002, 1, 1)
-        ddst = date.today()
-        table = []
-        for dt in rrule(DAILY, dtstart=dsrc, until=ddst):
-            #print('Fetching data for `{}` ...'.format(dt.strftime('%Y-%m-%d')))
-            table.extend(callback(dt))
-        df = pd.DataFrame(table,
-            columns=['reportDate', 'instrumentId', 'refSettlementPrice', 'updown',],)
-        df.to_csv(FUTURES_CSV)
-        return df
+        return row
 
-    def generateUrl(date, suffix):
-        if date is None:
-            date = datetime.today()
-        if suffix is None:
-            raise AssertionError('Parameter `suffix` is None!')
-        print('Input parameters:')
-        print('date=', date)
-        print('suffix=', suffix.value)
+    def parseText(self,
+                reportDate: datetime.date,
+                text: str,
+            ):
+        if not text:
+            raise TypeError('Argument `text` is not specified!')
 
-        if isinstance(suffix, Suffix):
+        result = json.loads(text)
+        chunks = result.get('o_currefprice')
+        if not isinstance(chunks, collections.Iterable):
+            raise SpiderException('Invalid data structure: invalid chunks found!')
+
+        for chunk in chunks:
+            if not isinstance(chunk, dict):
+                raise SpiderException('Invalid data structure: invalid chunk found!')
+
+            chunkTime = chunk.get('TIME')
+            if not isinstance(chunkTime, str):
+                raise SpiderException('Invalid data structure: invalid TIME found!')
+
+            if '9:00-15:00' in chunkTime:
+                row = self.parseChunk(reportDate, chunk)
+                self.table = self.table.append(row)
+
+    def parseData(self, reportDate, response):
+        try:
+            text = response.text
+            self.parseText(reportDate, text)
+
+        except SpiderException as e:
+            logger.info('------------------------------')
+            logger.info(text)
+            logger.info('------------------------------')
+            logger.exception(e)
+
+    def generateUrl(self, reportDate, suffix):
+        if not reportDate:
+            raise TypeError('Argument `reportDate` is not specified!')
+        if not suffix:
+            raise TypeError('Argument `suffix` is not specified!')
+
+        #logger.info(f'Input parameters:\ndate={reportDate}\nsuffix={suffix.value}\n')
+
+        if isinstance(suffix, SHFE.Suffix):
             suffix = suffix.value
+
         if isinstance(suffix, str):
-            result = 'http://www.shfe.com.cn/data/dailydata/ck/{}{}'.format(date.strftime('%Y%m%d'), suffix)
+            # Example:
+            # 'http://www.shfe.com.cn/data/dailydata/ck/20180920mainTimePrice.dat'
+            sReportDate = reportDate.strftime('%Y%m%d')
+            result = f'http://www.shfe.com.cn/data/dailydata/ck/{sReportDate}{suffix}'
             print('url=', result)
             return result
+
         else:
-            raise AssertionError('Parameter `suffix` is invalid!')
+            raise TypeError(f'Argument `suffix={suffix}` is invalid!')
+
+    def fetchData(self, session, reportDate, suffix):
+        url = self.generateUrl(reportDate, suffix)
+        response = session.get(url)
+
+        logger.info(f'Fetching data for `url={url}` ...')
+
+        if response.status_code == 200:
+            self.parseData(reportDate, response)
+
+        else:
+            logger.error(f'Fail to retrieve request(url={url})!')
+
+    def traverseDate(self,
+                callback,
+                dsrc: datetime.date = datetime.date(2002, 1, 1),
+                ddst: datetime.date = datetime.date.today(),
+            ):
+        if not isinstance(dsrc, datetime.date):
+            raise TypeError(f'Argument `dsrc` has invalid type: `{type(dsrc)}`!')
+        if not isinstance(ddst, datetime.date):
+            raise TypeError(f'Argument `ddst` has invalid type: `{type(ddst)}`!')
+        if not dsrc < ddst:
+            raise ValueError('Argument `dsrc` should be earlier than `ddst`!')
+        if not callback:
+            raise TypeError('Argument `callback` is not specified!')
+
+        for reportDate in rrule.rrule(rrule.DAILY, dtstart=dsrc, until=ddst):
+            callback(reportDate)
