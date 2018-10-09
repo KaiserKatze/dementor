46dd81d28c6bff8e0311c095eec8899e2410d9a5
KaiserKatze <donizyo@gmail.com>
2018-10-10 00:16:09 +0800

    Update

--- target.py
+++ target.py
@@ -3,10 +3,27 @@
 
 import json
 import concurrent.futures
+import os.path
+import sqlite3
+import logging
 
 import requests
+import pandas as pd
+import numpy as np
 
 from decorators import package
+
+############################################################################
+LOGGING_FILE = 'futures.log'
+logging.basicConfig(\
+        filename = LOGGING_FILE,
+        filemode = 'w',
+        level = logging.INFO,
+        format = '[%(asctime)s] %(levelname)s %(message)s',
+        datefmt = '%m/%d/%Y %I:%M:%S %p',
+    )
+logger = logging.getLogger(__name__)
+############################################################################
 
 DEFAULT_HTTP_REQUEST_HEADERS_CONFIG_PATH = 'http-request-headers.config.json'
 
@@ -22,7 +39,9 @@ def getHttpRequestHeadersConfig():
 
     return data
 
-def Session(host, referer):
+def Session(host: str = None,
+            referer: str = None,
+        ):
     data = getHttpRequestHeadersConfig()
 
     headers = {
@@ -41,10 +60,73 @@ def ThreadPoolExecutor():
     return concurrent.futures.ThreadPoolExecutor(max_workers = MAX_WORKERS)
 
 class Target:
-    def __init__(self):
-        self._session = None
+
+    DEFAULT_SQL_PATH = 'db.sqlite3'
+    DEFAULT_INDEX_NAME = 'index'
+
+    @classmethod
+    def GetDefaultTableName(cls):
+        return cls.__name__.lower()
+
+    @classmethod
+    def createNewTable(cls, columns):
+        return pd.DataFrame(\
+                columns = columns,
+                dtype = np.int64,
+            )
+
+    @classmethod
+    def loadTableFromSqlite(cls, tableName, columns):
+        try:
+            with sqlite3.connect(cls.DEFAULT_SQL_PATH) as conn:
+                columnNameSeperator = ', '
+                backquote = '`'
+                sIndexColumnName = cls.DEFAULT_INDEX_NAME
+                columns = [sIndexColumnName, *columns]
+                # wrap each column name with backquotes
+                backquotes = [backquote,] * 2
+                columns = map(lambda column: ''.join(column.join(backquotes)), columns)
+                sColumnNames = columnNameSeperator.join(columns)
+
+                df = pd.read_sql_query(\
+                        f'''SELECT {sColumnNames}
+                            FROM {tableName}''',
+                        conn,
+                        index_col = sIndexColumnName,
+                        parse_dates = [
+                            sIndexColumnName,
+                        ],
+                    )
+                # TODO validate data integrity
+
+                logger.info(f'''Load table successfully!
+                    Loaded table:\n{df}''')
+
+                return df
+
+        except pd.io.sql.DatabaseError as e:
+            logger.exception(e)
+            raise
+
+        except Exception as e:
+            logger.exception(e)
+            return None
+
+    def __init__(self,
+                session = Session(),
+                tableName = self.GetDefaultTableName(),
+            ):
+        self._session = session
         self._executor = ThreadPoolExecutor()
+        self._tableName = tableName
         self._table = None
+
+    def __del__(self):
+        try:
+            if self._session:
+                self._session.close()
+        except:
+            pass
 
     @property
     def session(self):
@@ -61,6 +143,46 @@ class Target:
     @table.setter
     def table(self, value):
         self._table = table
+
+    def loadTable(self,
+                columns: list,
+                force_new = False,
+            ):
+        if not columns:
+            raise SpiderException(TypeError('Argument `columns` is not specified!'))
+
+        if not force_new and os.path.isfile(self.DEFAULT_SQL_PATH):
+            tableName = self._tableName
+            self._table = self.loadTableFromSqlite(tableName, columns)
+
+        if self._table is None:
+            self._table = self.createNewTable(columns)
+
+        assert self._table is not None, 'Fail to load table!'
+
+        # Post-loading processing
+        self._table.index.name = self.DEFAULT_INDEX_NAME
+
+    def saveTable(self):
+        if self._table is None:
+            raise TypeError('Field `table` is None!')
+
+        with sqlite3.connect(self.DEFAULT_SQL_PATH) as conn:
+            tableName = self._tableName
+            self._table.to_sql(\
+                    tableName,
+                    conn,
+                    schema = None,
+                    if_exists = 'replace',
+                    index = True,
+                    index_label = self.DEFAULT_INDEX_NAME,
+                    chunksize = None,
+                    dtype = {
+                        'instrumentId': 'TEXT',
+                        'refSettlementPrice': 'INTEGER',
+                        'updown': 'INTEGER',
+                    },
+                )
 
 class BaseParser:
     pass
--- targets/shfe/futures.py
+++ targets/shfe/futures.py
@@ -1,30 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os.path
 import datetime
-import sqlite3
 import logging
 
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
 from target import Target, Session
-import spiders
-import parsers
+from parsers import TimePriceParser
+from spiders import TimePriceSpider
 
 ############################################################################
-LOGGING_FILE = 'futures.log'
-logging.basicConfig(\
-        filename = LOGGING_FILE,
-        filemode = 'w',
-        level = logging.INFO,
-        format = '[%(asctime)s] %(levelname)s %(message)s',
-        datefmt = '%m/%d/%Y %I:%M:%S %p',
-    )
 logger = logging.getLogger(__name__)
-DEFAULT_SEPERATOR = '------------------------------'
 ############################################################################
 
 def plot(dataframe, instrumentId):
@@ -36,32 +23,28 @@ def plot(dataframe, instrumentId):
         )
     plt.show()
 
-class SHFE(Target):
+class SHFE(Target, TimePriceParser, TimePriceSpider):
 
     HOSTNAME = 'www.shfe.com.cn'
     URL_REFERER = 'http://www.shfe.com.cn/statements/dataview.html?paramid=delaymarket_cu'
     DEFAULT_COLUMNS = ( 'instrumentId', 'refSettlementPrice', 'updown', )
-    DEFAULT_SQL_PATH = 'shfe.sqlite3'
-    DEFAULT_SQL_TABLE_NAME = 'shfe'
-    DEFAULT_INDEX_NAME = 'index'
 
     def __init__(self):
-        super().__init__()
+        super(Target, self).__init__(\
+                session = Session(\
+                        host = self.HOSTNAME,
+                        referer = self.URL_REFERER,
+                    ),
+            )
 
-        self.session = Session(host=SHFE.HOSTNAME, referer=SHFE.URL_REFERER)
-        self.spider = spiders.TimePriceSpider(self)
-        self.parser = parsers.TimePriceParser(self)
-
-    def __del__(self):
-        try:
-            self.session.close()
-
-        except:
-            pass
+    def loadTable(self,
+            force_new = False,
+        ):
+        super().loadTable(self.DEFAULT_COLUMNS, force_new,)
 
     def startSpider(self,
-                dsrc=datetime.date(2013, 12, 1),
-                ddst=datetime.date.today(),
+                dsrc: datetime.date = datetime.date(2013, 12, 1),
+                ddst: datetime.date = datetime.date.today(),
             ):
         sdsrc = dsrc.strftime('%Y-%m-%d')
         sddst = ddst.strftime('%Y-%m-%d')
@@ -69,89 +52,12 @@ class SHFE(Target):
 
         try:
             self.loadTable()
-            self.spider.traverseDate(\
+            self.traverseDate(\
                     dsrc = dsrc,
                     ddst = ddst,
                 )
-
         finally:
             self.saveTable()
-
-    def loadTable(self,
-                force_new=False,
-            ):
-        self.table = None
-
-        if not force_new and os.path.isfile(SHFE.DEFAULT_SQL_PATH):
-            self.table = self.loadTableFromSqlite()
-
-        if self.table is None:
-            self.table = self.createNewTable()
-
-        assert self.table is not None, 'Fail to load table!'
-
-        # Post-loading processing
-        self.table.index.name = SHFE.DEFAULT_INDEX_NAME
-
-    def createNewTable(self):
-        return pd.DataFrame(\
-                columns=self.DEFAULT_COLUMNS,
-                dtype=np.int64,
-            )
-
-    def loadTableFromSqlite(self):
-        try:
-            with sqlite3.connect(SHFE.DEFAULT_SQL_PATH) as conn:
-                columnNameSeperator = ', '
-                sIndexColumnName = SHFE.DEFAULT_INDEX_NAME
-                columns = [sIndexColumnName, *self.DEFAULT_COLUMNS]
-                # wrap each column name with
-                columns = map(lambda column: ''.join(column.join(['`',] * 2)), columns)
-                columnNames = columnNameSeperator.join(columns)
-                tableName = SHFE.DEFAULT_SQL_TABLE_NAME
-
-                df = pd.read_sql_query(\
-                        f'''SELECT {columnNames}
-                            FROM {tableName}''',
-                        conn,
-                        index_col = sIndexColumnName,
-                        parse_dates = [
-                            sIndexColumnName,
-                        ],
-                    )
-                # TODO validate data integrity
-
-                logger.info(f'Load table successfully!\n{DEFAULT_SEPERATOR}\nLoaded table:\n{df}\n{DEFAULT_SEPERATOR}')
-
-                return df
-
-        except pd.io.sql.DatabaseError as e:
-            logger.exception(e)
-            raise
-
-        except Exception as e:
-            logger.exception(e)
-            return None
-
-    def saveTable(self):
-        if self.table is None:
-            raise TypeError('Field `table` is None!')
-
-        with sqlite3.connect(SHFE.DEFAULT_SQL_PATH) as conn:
-            self.table.to_sql(\
-                    SHFE.DEFAULT_SQL_TABLE_NAME,
-                    conn,
-                    schema = None,
-                    if_exists = 'replace',
-                    index = True,
-                    index_label = SHFE.DEFAULT_INDEX_NAME,
-                    chunksize = None,
-                    dtype = {
-                        'instrumentId': 'TEXT',
-                        'refSettlementPrice': 'INTEGER',
-                        'updown': 'INTEGER',
-                    },
-                )
 
 if __name__ == '__main__':
     shfe = SHFE()
